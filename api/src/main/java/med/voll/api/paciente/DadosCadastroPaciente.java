f27f0f5dceb67f23ca93a9e018b7b62508380efb
Chrystian <chrystianfarias@hotmail.com>
2023-07-10 10:32:38 -0300

    fix - nova migration pacientes//anotacoes

--- api/src/main/java/med/voll/api/paciente/DadosCadastroPaciente.java
+++ api/src/main/java/med/voll/api/paciente/DadosCadastroPaciente.java
@@ -1,8 +1,10 @@
 package med.voll.api.paciente;
 
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import med.voll.api.endereco.DadosEndereco;
 
 public record DadosCadastroPaciente(
@@ -10,15 +12,15 @@ public record DadosCadastroPaciente(
 		@NotBlank
 		String nome,
 		
-		@NotBlank
+		@NotBlank  @Email 
 		String email,
 		
-		@NotBlank
+		@NotBlank 
 		String telefone,
 		
-		@NotBlank
+		@NotBlank @Pattern(regexp = "\\d{3}\\.?\\d{3}\\.?\\d{3}\\-?\\d{2}")
 		String cpf, 
-		
+			
 		@NotNull @Valid
 		DadosEndereco endereco) {
 	
