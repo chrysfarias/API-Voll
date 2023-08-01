983b2560229996a9896fdb1631c4c926c0d91538
Chrystian <chrystianfarias@hotmail.com>
2023-08-01 10:07:49 -0300

    feat - exclusão de médico

--- api/src/main/java/med/voll/api/controller/MedicoController.java
+++ api/src/main/java/med/voll/api/controller/MedicoController.java
@@ -1,13 +1,13 @@
 package med.voll.api.controller;
 
-import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties.Pageable;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.web.PageableDefault;
-import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,8 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
-import lombok.experimental.var;
-import med.voll.api.endereco.DadosListagemEnderecosMedicos;
+
 import med.voll.api.medico.DadosAtualizacaoMedico;
 import med.voll.api.medico.DadosCadastroMedico;
 import med.voll.api.medico.DadosListagemMedico;
@@ -44,19 +43,7 @@ public class MedicoController {
 		
 	}
 	
-	@GetMapping("/listarEnderecos")
-	public List<DadosListagemEnderecosMedicos> listarEnderecosDosMedicos(){
-		return repository.findAll().stream().map(DadosListagemEnderecosMedicos:: new).toList();
-		
-	}
 	
-	
-	@GetMapping("/listarTudo")
-	public List<Medico> listarMedicosComTodosOsAtributos(){
-		return repository.findAll();
-	}
-	
-
 	@PutMapping
 	@Transactional
 	public void atualizarMedicos(@RequestBody @Valid DadosAtualizacaoMedico dados) {
@@ -64,6 +51,13 @@ public class MedicoController {
 		Medico medico = new Medico();
 		medico = repository.getReferenceById(dados.id());
 		medico.atualizarInformacoes(dados);
+		
+	}
+	
+	@DeleteMapping("/{id}")
+	@Transactional
+	public void excluirMedico(@PathVariable Long id) {
+		repository.deleteById(id);
 		
 	}
 	
